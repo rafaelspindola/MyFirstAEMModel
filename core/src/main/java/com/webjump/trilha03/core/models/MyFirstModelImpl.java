81518e0fd3ca8725bcd04faa80b29bbd3ac060ec
Rafael Spíndola <rafa.spin95@gmail.com>
2023-08-07 21:51:16 -0300

    Adjusted html/htl file at ui.apps directory and corrected some code at MyFirstModelImpl

--- core/src/main/java/com/webjump/trilha03/core/models/MyFirstModelImpl.java
+++ core/src/main/java/com/webjump/trilha03/core/models/MyFirstModelImpl.java
@@ -5,28 +5,25 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+@Model(adaptables = Resource.class)
 public class MyFirstModelImpl {
 
-    @Model(adaptables = Resource.class)
-    public class MyFirstModel {
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    protected String clientName;
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    protected int numberID;
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    protected Boolean isNewClient;
 
-        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-        protected String clientName;
-        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-        protected int numberID;
-        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-        protected Boolean isNewClient;
+    public String getClientName() {
+        return clientName;
+    }
 
-        public String getClientName() {
-            return clientName;
-        }
+    public int getNumberID() {
+        return numberID;
+    }
 
-        public int getNumberID() {
-            return numberID;
-        }
-
-        public Boolean getIsNewClient() {
-            return isNewClient;
-        }
+    public Boolean getIsNewClient() {
+        return isNewClient;
     }
 }
