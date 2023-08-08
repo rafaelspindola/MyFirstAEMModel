e71deb326604881079dbf8c446bdebcac1f19627
Rafael Spíndola <rafa.spin95@gmail.com>
2023-08-07 21:19:39 -0300

    Created an interface for MyFirstModel

--- core/src/main/java/com/webjump/trilha03/core/models/MyFirstModel.java
+++ core/src/main/java/com/webjump/trilha03/core/models/MyFirstModel.java
@@ -8,18 +8,8 @@ import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 
-@Model(adaptables = Resource.class)
-public class MyFirstModel {
-
-    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
-    protected String clientName;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    protected int numberID;
-    @ValueMapValue(injectionStrategy=InjectionStrategy.OPTIONAL)
-    protected Boolean isNewClient;
-
-    public String getClientName() {return clientName;}
-
-    public int getNumberID() {return numberID;}
-    public Boolean getIsNewClient() {return isNewClient;}
+public interface MyFirstModel {
+    String getClientName();
+    int getNumberID();
+    Boolean getIsNewClient();
 }
--- core/src/main/java/com/webjump/trilha03/core/models/MyFirstModelImpl.java
+++ core/src/main/java/com/webjump/trilha03/core/models/MyFirstModelImpl.java
@@ -1,4 +1,32 @@
 package com.webjump.trilha03.core.models;
 
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
 public class MyFirstModelImpl {
+
+    @Model(adaptables = Resource.class)
+    public class MyFirstModel {
+
+        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+        protected String clientName;
+        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+        protected int numberID;
+        @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+        protected Boolean isNewClient;
+
+        public String getClientName() {
+            return clientName;
+        }
+
+        public int getNumberID() {
+            return numberID;
+        }
+
+        public Boolean getIsNewClient() {
+            return isNewClient;
+        }
+    }
 }
