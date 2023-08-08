3b9236016f26ddad3fee8c3ad3884237d724f243
Rafael Spíndola <rafa.spin95@gmail.com>
2023-08-08 16:03:39 -0300

    Created PayloadData class to handle payload from http requests

---
+++ core/src/main/java/com/webjump/trilha03/core/payload/PayloadData.java
@@ -0,0 +1,12 @@
+package com.webjump.trilha03.core.payload;
+public class PayloadData {
+    private String clientName;
+    private int numberID;
+    private Boolean isNewClient;
+
+    public String getClientName() {return clientName;}
+
+    public String getNumberID() {return numberID;}
+
+    public Boolean getIsNewClient() {return isNewClient;}
+}
